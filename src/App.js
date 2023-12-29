055054998ca4bf6db1b003220d388a1b64a05eae
MakarevichAV <makarevich.a.v@yandex.ru>
2023-12-29 10:51:18 +0300

    Директория для beckend, инициализация проекта, установлены зависимости. Добавлен .gitignore для игнорирования node_modules в backend

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import LoginPage from './pages/LoginPage'; // Добавьте импорт
 
 const App = () => {
 
   return (
     <div>
-      {/* <h1>Count: {count}</h1>
-      <StyledButton onClick={() => dispatch(increment())}>Increment</StyledButton>
-      <StyledButton onClick={() => dispatch(decrement())}>Decrement</StyledButton> */}
-
-    
       <LoginPage />
     </div>
   );
