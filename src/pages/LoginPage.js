d503e334344e75f16195359ed3ef65eb378cf649
MakarevichAV <makarevich.a.v@yandex.ru>
2024-01-05 16:11:07 +0300

    save

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -27,6 +27,9 @@ const LoginPage = () => {
         <img src="/logo.svg" alt="Logo" />
       </div>
       <div className="login-container">
+      {successMessage && (
+        <div className="success-message">{successMessage}</div>
+      )}
         <h2>{isLogin ? 'Login' : 'Register'}</h2>
         {isLogin ? (
           <LoginForm onSubmit={handleFormSubmit} />
@@ -35,16 +38,6 @@ const LoginPage = () => {
         )}
         <p onClick={toggleMode}>{isLogin ? 'Need to register?' : 'Already have an account?'}</p>
       </div>
-      {/* Выводим данные из формы */}
-      {Object.keys(formData).length > 0 && (
-        <div>
-          <h3>Form Data:</h3>
-          <pre>{JSON.stringify(formData, null, 2)}</pre>
-        </div>
-      )}
-      {successMessage && (
-        <div style={{ color: 'green', margin: '10px 0' }}>{successMessage}</div>
-      )}
     </div>
   );
 };
