2e2ada7831778cf0908740c7a79f3d7b478e5e47
MakarevichAV <makarevich.a.v@yandex.ru>
2024-01-14 23:18:07 +0300

    add edit save delete directory success

--- src/redux/reducers/directoryReducer.js
+++ src/redux/reducers/directoryReducer.js
@@ -2,19 +2,6 @@ const initialState = [];
 
 const directoryReducer = (state = initialState, action) => {
     switch (action.type) {
-        // case 'EDIT_DIRECTORY':
-        //     return state.map((directory) =>
-        //         directory.id === action.payload.id
-        //             ? { ...directory, isEditing: true, editedName: action.payload.editedName, editedImage: action.payload.editedImage }
-        //             : directory
-        //     );
-
-        // case 'SAVE_DIRECTORY':
-        //     return state.map((directory) =>
-        //         directory.id === action.payload.id
-        //             ? { ...directory, isEditing: false, name: action.payload.editedName, image: action.payload.editedImage } 
-        //             : directory
-        //     );
         case 'SAVE_DIRECTORY':
             const updatedState = state.map((directory) =>
                 directory._id === action.payload.id
@@ -24,14 +11,14 @@ const directoryReducer = (state = initialState, action) => {
             return updatedState;
 
         case 'DELETE_DIRECTORY':
-            return state.filter((directory) => directory.id !== action.payload.id);
+            return state.filter((directory) => directory._id !== action.payload.id);
 
         case 'ADD_DIRECTORY':
             // Добавляем новую директорию в режиме редактирования
             return [
                 ...state,
                 {
-                    id: action.payload._id, // Используем возвращенный ID от сервера
+                    _id: action.payload._id, // Используем возвращенный ID от сервера
                     name: '',
                     image: '',
                     setsCount: 0,
