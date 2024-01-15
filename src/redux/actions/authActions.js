f26b76dc49d8225b526fb000eaad4883bb3dc61c
MakarevichAV <makarevich.a.v@yandex.ru>
2024-01-15 23:01:38 +0300

    saving user data in local storage is success

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -6,9 +6,15 @@ export const login = ({ username, password }) => async (dispatch) => {
   try {
     // Make the API call to your backend /login endpoint
     const response = await axios.post(`${serverUrl}/login`, { username, password });
-console.log(response);
+    // console.log(response.data.user);
     // Dispatch an action for successful login
     dispatch({ type: 'LOGIN_SUCCESS', payload: response.data });
+    const user = {
+      userId: response.data.user._id,
+      username: response.data.user.username,
+    };
+    // сохранение данных пользователя в local storage
+    localStorage.setItem('user', JSON.stringify(user));
   } catch (error) {
     // Dispatch an action for login failure
     dispatch({ type: 'LOGIN_FAILURE', payload: error.message });
@@ -16,10 +22,10 @@ console.log(response);
 };
 
 export const register = ({ username, password }) => async (dispatch) => {
-    try {
-      const response = await axios.post(`${serverUrl}/register`, { username, password });
-      dispatch({ type: 'REGISTER_SUCCESS', payload: response.data });
-    } catch (error) {
-      dispatch({ type: 'REGISTER_FAILURE', payload: error.message });
-    }
-  };
+  try {
+    const response = await axios.post(`${serverUrl}/register`, { username, password });
+    dispatch({ type: 'REGISTER_SUCCESS', payload: response.data });
+  } catch (error) {
+    dispatch({ type: 'REGISTER_FAILURE', payload: error.message });
+  }
+};
--- src/redux/actions/directoryActions.js
+++ src/redux/actions/directoryActions.js
@@ -14,15 +14,11 @@ export const addDirectory = () => {
 
             // Получение информации о пользователе из состояния
             const auth = getState().auth;
-            // console.log(auth);
+console.log(auth);
             // Отправка запроса к серверу для сохранения данных в базе данных
             const response = await axios.post(`${serverUrl}/addDirectory`, {
                 ...newDirectory,
-                owner: {
-                    userId: auth.userId, // Передача ID пользователя в запросе
-                    ref: 'User',
-                    required: true,
-                }
+                userId: auth.userId, // Передача ID пользователя в запросе
             });
 
             dispatch({
