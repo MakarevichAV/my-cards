f328ab3964810b8340bc5b4ec10f961fff93e4bd
MakarevichAV <makarevich.a.v@yandex.ru>
2024-01-17 23:13:35 +0300

    api подключена, получаю картинки в popup

--- src/components/SearchPopup.js
+++ src/components/SearchPopup.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import '../styles/Popup.css';
 
-const SearchPopup = ({ onClose, onSearch, searchResults }) => {
+const SearchPopup = ({ onClose }) => {
+    const [searchQuery, setSearchQuery] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleSearch = async () => {
+        try {
+            const response = await axios.get(
+                `https://pixabay.com/api/?key=33256265-9311bbeda59bd85787262c6fb&q=${encodeURIComponent(searchQuery)}&image_type=vector`
+            );
+
+            setSearchResults(response.data.hits);
+        } catch (error) {
+            console.error('Error searching images:', error);
+        }
+    };
+
     return (
         <div className="search-popup">
             <div className="search-header">
-                <input type="text" onChange={(e) => onSearch(e.target.value)} placeholder="Поиск" />
+                <input
+                    type="text"
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    placeholder="Поиск"
+                />
+                <button onClick={handleSearch}>Искать</button>
                 <div className="close-icon" onClick={onClose}></div>
             </div>
             <div className="search-results">
-
-                {/* {searchResults.map((result) => (
-                    <div key={result.id}>{result.name}</div>
-                ))} */}
-
+                {searchResults.length > 0 ? (
+                    searchResults.map((result) => (
+                        <div className="search-item" key={result.id} style={{
+                            backgroundImage: `url(${result.previewURL})`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center'
+                        }}>
+                            {/* <img src={result.previewURL} alt={result.tags} /> */}
+                        </div>
+                    ))
+                ) : (
+                    <p>Нет результатов поиска</p>
+                )}
             </div>
         </div>
     );
