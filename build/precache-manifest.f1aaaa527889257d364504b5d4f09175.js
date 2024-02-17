7b3eab0b38a313ee64156829625aecdcc052a50e
MakarevichAV <makarevich.a.v@yandex.ru>
2024-02-17 22:06:56 +0300

    build for deploy testing on netlify

---
+++ build/precache-manifest.f1aaaa527889257d364504b5d4f09175.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7451eabdccd994b3d7b634cf1aedeb8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "055b201567d77b72462e",
+    "url": "/static/css/2.cc82f8aa.chunk.css"
+  },
+  {
+    "revision": "8a6e2e7e5d5de693d1a0",
+    "url": "/static/css/main.69342869.chunk.css"
+  },
+  {
+    "revision": "055b201567d77b72462e",
+    "url": "/static/js/2.4e7ca2a7.chunk.js"
+  },
+  {
+    "revision": "5ebab883716e8cc492f3732669b8bbff",
+    "url": "/static/js/2.4e7ca2a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a6e2e7e5d5de693d1a0",
+    "url": "/static/js/main.701d9dd6.chunk.js"
+  },
+  {
+    "revision": "bace152efcc605b71b08",
+    "url": "/static/js/runtime-main.0fa6cbeb.js"
+  },
+  {
+    "revision": "3f226a5e29497cb6a3589719d72fa7fb",
+    "url": "/static/media/add-button.3f226a5e.png"
+  },
+  {
+    "revision": "e0c56af7ab92c20304aa39663c9373f6",
+    "url": "/static/media/camera.e0c56af7.png"
+  },
+  {
+    "revision": "9e906607c9afc3dd7f066e3599ca2a75",
+    "url": "/static/media/edit.9e906607.png"
+  },
+  {
+    "revision": "fcfa25e9f1dd8508515f1623e6ad9b42",
+    "url": "/static/media/exit.fcfa25e9.svg"
+  },
+  {
+    "revision": "36d86d987438ccffd8096903812a361e",
+    "url": "/static/media/left-arrow.36d86d98.png"
+  },
+  {
+    "revision": "6521bd74a7569ab419d2ed27956a4a09",
+    "url": "/static/media/max-logo.6521bd74.svg"
+  },
+  {
+    "revision": "5f066b72d3f24aad30f433273360eb2e",
+    "url": "/static/media/save-disact.5f066b72.png"
+  },
+  {
+    "revision": "30ee5e4fd9a96aa0daaa7c5598b30105",
+    "url": "/static/media/save.30ee5e4f.png"
+  }
+]);
